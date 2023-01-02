203f6b067ecb40789d1770da395252e652f52c15
071yoon <yeonggilol@gmail.com>
2023-01-02 22:03:44 +0900

    chore: remove unused vars

--- components/finder/FinderWindows.tsx
+++ components/finder/FinderWindows.tsx
@@ -3,7 +3,7 @@ import { folderData } from "../../utils/folder-data";
 import folderStore from "../../stores/folderStore";
 
 export default function FinderWindows() {
-  const { folders, addFolder, removeFolder } = folderStore();
+  const { folders, removeFolder } = folderStore();
   return (
     <>
       {folders.map((singleFolder, index) => {
